0d08e7f148a2c0fbbfd286c0718c025949378981
luccaki <lucca.ianaguivara@gmail.com>
2020-11-06 12:22:23 -0300

    att

--- todo-app/backend/src/config/routes.js
+++ todo-app/backend/src/config/routes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 
-module.exports = function(server){
+module.exports = function(server) {
 
-    //API Routes
+    // API Routes
     const router = express.Router()
     server.use('/api', router)
 
-    //TODO Routes
+    // TODO Routes
     const todoService = require('../api/todo/todoService')
     todoService.register(router, '/todos')
 }
