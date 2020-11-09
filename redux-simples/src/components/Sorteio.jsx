3ca5bebe7fdb4ca2033c0fa628a033bf5513c7d5
luccaki <lucca.ianaguivara@gmail.com>
2020-11-09 12:39:03 -0300

    redux

---
+++ redux-simples/src/components/Sorteio.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './Card'
+
+export default props => {
+    return (
+        <Card title="Sortei de um Número" purple>
+            <div>
+                <span>
+                    <span>Resultado:</span>
+                    <strong>{10}</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
